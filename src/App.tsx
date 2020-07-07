3eac843782700ef53f5ab5c85e2b214c78e8268a
anilson <anilsonmonteiro@gmail.com>
2020-07-07 15:02:16 -0100

    initial react projet setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './Styles/GlobalStyles';
+
+function App() {
+  return (
+     <>
+      <div>Hello</div>
+      <GlobalStyles/>
+     </>
+  );
+}
+
+export default App;
